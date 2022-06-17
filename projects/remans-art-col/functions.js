dbf6714fba419befd14f4cc9b9236c653c59c81e
wof12345 <lsampercot@gmail.com>
2022-06-17 14:51:01 +0600

    Final push

--- projects/remans-art-col/functions.js
+++ projects/remans-art-col/functions.js
@@ -1,7 +1,19 @@
+let mainBodyCards = document.querySelector(`.featured_main_body`);
 let details_art_page = document.querySelector(".art_details");
 let details_art_page_back = document.querySelector(".back_details");
 let imageAngles = document.querySelectorAll(".angle_pics");
 let mainDetailsImage = document.querySelector(".main_pic");
+let showPage = document.querySelector(`.showcase_page`);
+let backShowPgae = document.querySelector(`.showpage_back`);
+let showPic = document.querySelector(`.show_img`);
+let cardIndicator = document.querySelector(`.cart_indicator`);
+
+let pageUserData = {
+  cart: [],
+};
+
+//driver
+fillMainBody(6);
 
 document.addEventListener("click", (e) => {
   let targetElm = e.target;
@@ -11,10 +23,24 @@ document.addEventListener("click", (e) => {
   if (ElmData) {
     invokeDetailsPage(ElmData);
   }
+
+  if (targetElmClass === "show_page_link") {
+    let mainPic = document.querySelector(`.main_pic`).src;
+    invokeShowPage(mainPic);
+  }
+
+  if (targetElmClass === "add_to_cart" || targetElmClass === "buy_btn") {
+    pageUserData.cart.push(targetElm);
+    cardIndicator.innerHTML = pageUserData.cart.length;
+  }
 });
 
 details_art_page_back.addEventListener("click", (e) => {
   detainDetailsPage();
+});
+
+backShowPgae.addEventListener("click", () => {
+  detainShowPage();
 });
 
 function invokeDetailsPage(src) {
@@ -28,4 +54,72 @@ function invokeDetailsPage(src) {
 
 function detainDetailsPage() {
   details_art_page.style = "";
+}
+
+function invokeShowPage(src) {
+  showPage.style = "left:0";
+  showPic.src = src;
+}
+
+function detainShowPage() {
+  showPage.style = "";
+}
+
+function fillMainBody(number) {
+  let genHtml = "";
+  for (let i = 0; i < number; i++) {
+    genHtml += componentFunctionFeatureCard(i, "./remans-col/Painting 2.png");
+  }
+
+  mainBodyCards.innerHTML = genHtml;
+}
+
+function componentFunctionFeatureCard(number, src_context) {
+  return `<div class="feature_card" id = ${number} data-src="${src_context}">
+  <div class="img_part" data-src="${src_context}">
+    <img
+      src="${src_context}"
+      data-src="${src_context}"
+      alt=""
+    />
+  </div>
+  <div class="feature_desc" data-src="${src_context}">
+    <p class="feature_title" data-src="${src_context}">
+      J Resistance
+    </p>
+    <p
+      class="feature_artist"
+      data-src="${src_context}"
+    >
+      Ria Arante
+    </p>
+    <p class="feature_date" data-src="${src_context}">
+      Gouache On paper
+    </p>
+    <p
+      class="feature_source hidden"
+      data-src="${src_context}"
+    >
+      <i data-src="${src_context}">
+        Oil on Canvas, 2008
+      </i>
+    </p>
+    <p
+      class="feature_size hidden"
+      data-src="${src_context}"
+    >
+      <i data-src="${src_context}"> 23 x 21 in</i>
+    </p>
+  </div>
+  <div
+    class="feature_pricing hidden"
+    data-src="${src_context}"
+  >
+    <p class="price" data-src="${src_context}">$664</p>
+    <div class="shop_icons" data-src="${src_context}">
+      <img src="./remans-col/button-share.png" alt="" />
+      <img class="buy_btn" src="./remans-col/button-buy.png" alt="" />
+    </div>
+  </div>
+</div>`;
 }
