c7f13af0bdb1ffdb4cbc7133b77d2da393d2d1ca
wof12345 <lsampercot@gmail.com>
2025-02-07 23:10:40 +0600

    feat : logo

--- frontend/src/lib/components/Navigation/Navigation.svelte
+++ frontend/src/lib/components/Navigation/Navigation.svelte
@@ -1,9 +1,13 @@
 <script>
+	import Logo from '$lib/components/Logo/Logo.svelte';
+
 	let { children } = $props();
 </script>
 
 <div class="">
-	<div class="h-[100px] w-full bg-black"></div>
+	<div class="h-[100px] w-full bg-black">
+		<Logo />
+	</div>
 
 	{@render children()}
 </div>
