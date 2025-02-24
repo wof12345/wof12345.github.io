ff931110485ae8a85d6a92daf4369f72ff68570a
wof12345 <lsampercot@gmail.com>
2025-02-24 18:21:50 +0600

    progress : nav component

--- frontend/src/lib/components/Logo/Logo.svelte
+++ frontend/src/lib/components/Logo/Logo.svelte
@@ -1,2 +1,6 @@
 <script></script>
 
+<div class="text-4xl font-bold text-white">
+	<span class=""> D</span>
+	<span class="-ml-4">L</span>
+</div>
--- frontend/src/lib/components/Navigation/Navigation.svelte
+++ frontend/src/lib/components/Navigation/Navigation.svelte
@@ -4,10 +4,28 @@
 	let { children } = $props();
 </script>
 
-<div class="">
-	<div class="h-[100px] w-full bg-black">
-		<Logo />
-	</div>
+<div class="nav flex h-[100px] w-full items-center justify-between bg-black px-4">
+	<Logo />
 
-	{@render children()}
+	<div class="nav-items">
+		<ul class="text-md flex gap-5">
+			<li>
+				<a href="/" class="text-white">Home</a>
+			</li>
+			<li>
+				<a href="/#projects" class="text-white">Projects</a>
+			</li>
+			<li>
+				<a href="/#about" class="text-white">about</a>
+			</li>
+		</ul>
+	</div>
 </div>
+
+<div class='nav-sidebar h-screen w-screen fixed left-0 top-0'>
+	<div class=bg-gra></div>
+
+	<div class="h-full w-72 bg-black relative z-10"></div>
+</div>
+
+{@render children()}
